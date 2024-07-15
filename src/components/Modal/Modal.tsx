c64642c932742f14a88dd62c870a4e015b5b60b4
Justin <cruzjustin1428@gmail.com>
2024-07-15 15:57:41 -0600

    Componente Modal Reutilizable

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { VscChromeClose } from "react-icons/vsc";
+
+interface Modal {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<Modal> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto">
+          <div className="fixed inset-0 bg-gray-900 bg-opacity-50 transition-opacity" onClick={onClose}></div>
+          <div className="relative bg-white rounded-lg shadow-lg max-w-lg w-full mx-auto">
+        <div>
+          {children}
+          <button
+              onClick={onClose}
+              className="absolute top-0 right-0 mt-4 mr-4 text-white hover:text-gray-400"
+            >
+              <VscChromeClose className="h-6 w-6" />
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
