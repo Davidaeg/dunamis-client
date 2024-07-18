caa60bade18067c52080d238413f148517e5e971
Justin <cruzjustin1428@gmail.com>
2024-07-18 11:37:56 -0600

    Actualizacion para devolver una señal de que se hizo el delete

--- src/hooks/persona/useDeletePersonas.ts
+++ src/hooks/persona/useDeletePersonas.ts
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import { dunamisApi } from "../../datasources/dunamisApi.service";
 
-
 export const useDeletePersonas = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  const deletePersona = async (idPersona: string): Promise<void> => {
+  const deletePersona = async (idPersona: string): Promise<boolean> => {
     setLoading(true);
     try {
       await dunamisApi.delete(`/persona/${idPersona}`);
       setLoading(false);
+      return true;
     } catch (err) {
       setError("Error deleting persona");
       setLoading(false);
+      return false;
     }
   };
 
   return { deletePersona, loading, error };
 };
+
 
