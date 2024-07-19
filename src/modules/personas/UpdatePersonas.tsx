f97b4f4ed6af89064a645ceb7ea3fb683f182eec
Justin <cruzjustin1428@gmail.com>
2024-07-18 22:46:46 -0600

    Agregar que el ID no se pueda modificar

--- src/modules/personas/UpdatePersonas.tsx
+++ src/modules/personas/UpdatePersonas.tsx
@@ -55,6 +55,7 @@ const UpdatePersonas: React.FC<UpdatePersonasProps> = ({ persona, onUpdate }) =>
               placeholder="ID Persona"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               required
+              readOnly
             />
           </div>
           <div>
