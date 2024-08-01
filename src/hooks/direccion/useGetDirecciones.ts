299c0308ee120e25b76c977b4f2abed53a736a70
Justin <cruzjustin1428@gmail.com>
2024-07-31 23:41:38 -0600

    Modificacion para obtener ambas tablas de persona y direccion mediante la coliumna idPersona

--- src/hooks/direccion/useGetDirecciones.ts
+++ src/hooks/direccion/useGetDirecciones.ts
@@ -1,28 +1,36 @@
 import { useState, useEffect } from "react";
-import { DireccionDB } from "../../modules/personas/persona.types";
+import { DireccionDB, PersonaDB } from "../../modules/personas/persona.types";
 import { dunamisApi } from "../../datasources/dunamisApi.service";
 
-export const useGetDirecciones = () => {
+export const useGetDirecciones = (idPersona: string) => {
   const [direccion, setDirecciones] = useState<DireccionDB[]>([]);
+  const [persona, setPersona] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   const fetchDirecciones = async () => {
     setLoading(true);
     try {
-      const response = await dunamisApi.get<DireccionDB[]>("/direcciones");
+      const response = await dunamisApi.get<DireccionDB[]>(`/direccionPersona/${idPersona}`);
+      const responsePersona = await dunamisApi.get<PersonaDB[]>(`/persona/${idPersona}`);
+      console.log('Datos obtenidos:', response.data);
       setDirecciones(response.data);
-      setLoading(false);
+      setPersona(responsePersona.data);
     } catch (err) {
+      console.error('Error fetching data:', err);
       setError("Error fetching user data");
+    } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchDirecciones();
-  }, []);
+    if (idPersona) {
+      fetchDirecciones();
+    }
+  }, [idPersona]);
 
-  return { direccion, loading, error, refetch: fetchDirecciones };
+  return { direccion, persona, loading, error, refetch: fetchDirecciones };
 };
+
 
